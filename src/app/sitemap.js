b4ed1e8a4cd31067de15c84a2962b6d4d7a4137a
yacinTech <jeenmatheo2@gmail.com>
2026-03-15 01:06:29 +0000

    first commit

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,23 +1,30 @@
-// app/sitemap.js
 export default function sitemap() {
   const baseUrl = "https://brandora.fun";
 
   return [
     {
       url: baseUrl,
-      lastModified: new Date("2026-03-11"),
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1,
     },
     {
       url: `${baseUrl}/services`,
-      lastModified: new Date("2026-03-01"),
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
     },
     {
       url: `${baseUrl}/a-propos`,
-      lastModified: new Date("2026-02-20"),
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.7,
     },
     {
       url: `${baseUrl}/contact`,
-      lastModified: new Date("2026-02-25"),
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.5,
     },
   ];
 }
