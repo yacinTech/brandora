0bf95913006a3971f776c8f530a2a408de5eb957
yacinTech <jeenmatheo2@gmail.com>
2026-03-23 11:50:47 +0100

    add sitemap

---
+++ src/app/robots.tsx
@@ -0,0 +1,14 @@
+// app/robots.ts
+import { MetadataRoute } from 'next';
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+      },
+    ],
+    sitemap: 'https://brandora.fun/sitemap.xml',
+  };
+}
--- src/app/sitemap.tsx
+++ src/app/sitemap.tsx
@@ -1,4 +1,7 @@
-export default function sitemap() {
+// app/sitemap.ts
+import { MetadataRoute } from 'next';
+
+export default function sitemap(): MetadataRoute.Sitemap {
   const baseUrl = "https://brandora.fun";
 
   return [
@@ -14,7 +17,7 @@ export default function sitemap() {
       changeFrequency: "monthly",
       priority: 0.8,
     },
-      {
+    {
       url: `${baseUrl}/projects`,
       lastModified: new Date(),
       changeFrequency: "monthly",
