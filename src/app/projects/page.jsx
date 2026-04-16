17f65b113e62783c7892a79d34743f46dbb90e7c
yacinTech <jeenmatheo2@gmail.com>
2026-04-16 16:48:23 +0100

    first commit

--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -1,4 +1,4 @@
-"use client";
+
 import PortfolioSection from "../../components/Portfolio/Portfolio";
 import Testimonials from "../../components/Testimonials/Testimonials";
 import ContactForm from "../../components/ContactForm";
